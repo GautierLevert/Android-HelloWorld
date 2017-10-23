721ff96a24ec5e96bd2c3ccec19fa143334d36d5
Gautier Levert <gautier.levert@gmail.com>
2017-10-23 16:11:20 +0200

    Add strings values

--- app/src/main/java/fr/iut_amiens/helloworld/MainActivity.java
+++ app/src/main/java/fr/iut_amiens/helloworld/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -17,13 +16,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-        editText = (EditText) findViewById(R.id.editText);
+        bindViews();
 
         button.setOnClickListener(this);
+    }
+
+    private void bindViews() {
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
     }
 
     @Override
